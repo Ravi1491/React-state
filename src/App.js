3da22232dbe95e3400d758c8437fbc09742993b8
Ravi Singh <ravi149185@gmail.com>
2024-02-15 21:43:19 +0530

    Add functionality to display random animal emoji

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
+function pickAnimal() {
+  const animals = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐨", "🐯"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+  const [animal, setAnimal] = useState("");
+
+  const handleOnClick = () => {
+    const newAnimal = pickAnimal();
+    setAnimal([...animal, newAnimal]);
+  };
+
   return (
     <div>
-      <h1> Number of Animals</h1>
+      <button onClick={handleOnClick}>Add Animals Emoji</button>
+      <h1> Animals Emoji = {animal}</h1>
     </div>
   );
 }
